f40718f500a1674382a83823d74fef306cd5f36f
SanRocks1220 <xxsantirocha12xx@gmail.com>
2023-12-03 16:18:29 -0500

    Ventana, pantalla de subir, terminadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,69 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
- const [repoLink, setRepoLink] = useState('');
- const [result, setResult] = useState<boolean | null>(null);
+  const [username, setUsername] = useState('');
+  const [gameLink, setGameLink] = useState('');
+  const [result, setResult] = useState<boolean | null>(null);
 
- const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await axios.post('/check-repo', { repoLink });
+      const response = await axios.post('/check-game', { username, gameLink });
       setResult(response.data);
     } catch (error) {
       console.error('Error:', error);
     }
- };
+  };
 
- return (
+  return (
     <div className="App">
+      <header>
+        <div className="header-content">
+          <div className="logo-container">
+            <img
+              src="/JDSGameHubLogo.png"
+              alt="JDS GameHub Logo"
+              className="logo"
+              style={{ width: '150px', height: 'auto' }}
+            />
+          </div>
+          <div className="project-name">
+            <h1 style={{ fontFamily: 'Orbitron, sans-serif' }}>JDS GameHub</h1>
+          </div>
+        </div>
+      </header>
       <form onSubmit={handleSubmit}>
-        <label>
-          Repo Link:
-          <input type="text" value={repoLink} onChange={e => setRepoLink(e.target.value)} />
-        </label>
-        <button type="submit">Check Repo</button>
+      <h2>Add Github Project</h2>
+      <label >
+        Your Name:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Enter your Github username"
+        />
+      </label>
+      <label>
+        Project Name:
+        <input
+          type="text"
+          value={gameLink}
+          onChange={(e) => setGameLink(e.target.value)}
+          placeholder="Paste the name of your Github project"
+        />
+      </label>
+      <button type="submit" style={{ fontFamily: 'Orbitron, sans-serif' }}>Submit Project</button>
       </form>
-      {result !== null && <p>Dockerfile and docker-compose.yml exist: {result.toString()}</p>}
+      {result !== null && (
+        <p>Your project has been submitted successfully: {result.toString()}</p>
+      )}
+      <footer>
+        <p>&copy; 2023 JDS GameHub. All rights reserved.</p>
+      </footer>
     </div>
- );
+  );
 }
 
 export default App;
+
+
